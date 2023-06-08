d4ce06cafb44f66765d264553b9567e05ac122f6
dathan <dathanvp@gmail.com>
2023-06-08 16:45:28 -0700

    feat: support safely deleting rows from the database as an inital version

--- cmd/go-safely-change-data/main.go
+++ cmd/go-safely-change-data/main.go
@@ -1,7 +1,125 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("hello world1")
+	// Define and parse command line flags
+	dbName := flag.String("db", "yourdb", "Name of the DB")
+	tableName := flag.String("table", "", "Name of the table to delete rows from")
+	whereClause := flag.String("where", "1=1", "WHERE clause to filter rows to delete")
+	flag.Parse()
+
+	if *tableName == "" {
+		log.Fatal("Please provide a table name using --table flag")
+	}
+
+	// Open a connection to the database
+	dsn := fmt.Sprintf("root:@tcp(localhost:3306)/%s", *dbName)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Determine the columns of the composite primary key
+	rows, err := db.Query(`SELECT COLUMN_NAME
+                           FROM information_schema.KEY_COLUMN_USAGE
+                           WHERE TABLE_NAME = ?
+                             AND CONSTRAINT_NAME = 'PRIMARY'`, *tableName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var primaryKeys []string
+	for rows.Next() {
+		var key string
+		if err := rows.Scan(&key); err != nil {
+			log.Fatal(err)
+		}
+		primaryKeys = append(primaryKeys, key)
+	}
+
+	// Query all rows needed to delete
+	sql := fmt.Sprintf("SELECT * FROM %s WHERE %s", *tableName, *whereClause)
+	log.Println(sql)
+	rows, err = db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Prepare the delete statement
+	whereConditions := make([]string, len(primaryKeys))
+	for i, key := range primaryKeys {
+		whereConditions[i] = fmt.Sprintf("%s = ?", key)
+	}
+	deleteSQL := fmt.Sprintf("DELETE FROM %s WHERE %s", *tableName, strings.Join(whereConditions, " AND "))
+	stmt, err := db.Prepare(deleteSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	// Concurrency control
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, 5) // Limit to 5 concurrent deletions
+
+	// Iterate over the rows
+	cols, err := rows.Columns()
+	if err != nil {
+		log.Fatal(err)
+	}
+	values := make([]interface{}, len(cols))
+	valuePtrs := make([]interface{}, len(cols))
+	for i := range values {
+		valuePtrs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err := rows.Scan(valuePtrs...)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Extract primary key values
+		var primaryKeyValues []interface{}
+		for _, key := range primaryKeys {
+			for i, col := range cols {
+				if col == key {
+					primaryKeyValues = append(primaryKeyValues, values[i])
+					break
+				}
+			}
+		}
+
+		// Delete row by composite primary key concurrently
+		wg.Add(1)
+		semaphore <- struct{}{}
+		go func(values []interface{}) {
+			defer wg.Done()
+			_, err := stmt.Exec(values...)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Deleted row with %s\n", strings.Join(whereConditions, " AND "))
+			<-semaphore
+		}(primaryKeyValues)
+	}
+
+	wg.Wait()
+
+	// Check for errors from iterating over rows
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
